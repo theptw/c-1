fcf38c66e6423c1be218bfe032f4463b3bfcb4f2
Nikita <nikita.bykov485@yandex.com>
2022-06-27 17:55:55 +0500

    Task 1 Square

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿//Квадрат целого числа
+Console.Write("Введите число ");
+
+int num = Convert.ToInt32(Console.ReadLine() );
+int square = num*num;
+
+
+Console.Write($"Квадрат числа {num} = {square} ");
